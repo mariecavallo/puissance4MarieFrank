18e8b6cea89d1d79bd24f819b3f1b13df51c9d88
FrankSofteam <contact@cadaux.fr>
2018-07-18 11:15:01 +0200

    maj class controle

--- src/main/fr/formation/puissance4/Controle.java
+++ src/main/fr/formation/puissance4/Controle.java
@@ -1,4 +1,25 @@
 package fr.formation.puissance4;
 
+import javafx.scene.paint.Color;
+
 public class Controle {
+
+
+    public boolean controle1a7(int choixColonne) {
+        if (choixColonne >= 1 || choixColonne <= 7) {
+            return true;
+        } else {
+            System.out.println("Saisir entre 1 et 7");
+            return false;
+        }
+    }
+
+    public boolean controlePionDessous(int ligne, int colonne){
+        if (ligne != 6)
+            if (Color.RED.equals(color) || (Color.YELLOW.equals(color)
+
+
+    }
+
+
 }
