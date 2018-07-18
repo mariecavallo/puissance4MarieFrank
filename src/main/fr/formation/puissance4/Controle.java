43b9d13b53e0a7f8814c668b2e6485cabe2ddb02
FrankSofteam <contact@cadaux.fr>
2018-07-18 16:11:13 +0200

    controle pion

--- src/main/fr/formation/puissance4/Controle.java
+++ src/main/fr/formation/puissance4/Controle.java
@@ -1,5 +1,8 @@
 package fr.formation.puissance4;
 
+import fr.formation.puissance4.Board.Board;
+import fr.formation.puissance4.Joueur.JoueurHumain;
+import fr.formation.puissance4.Piece.Jeton;
 import javafx.scene.paint.Color;
 
 public class Controle {
@@ -14,12 +17,32 @@ public class Controle {
         }
     }
 
-    public boolean controlePionDessous(int ligne, int colonne){
-        if (ligne != 6)
-            if (Color.RED.equals(color) || (Color.YELLOW.equals(color)
 
+
+    // retour true si le pion du dessous est vide
+
+    public boolean controlePionDessous(Board lesJetons, int ligne, int colonne) {
+        if (ligne != 6 && lesJetons.getJetons()[ligne - 1][colonne].getColor().equals(Color.TRANSPARENT)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+// retour true si la grille est pleine
+
+    public boolean controleGrillePleine(Board lesJetons,int ligne, int colonne) {
+        for (int j = 0; j < 7; j++) {
+            if (lesJetons.getJetons()[ligne - 1][colonne].getColor().equals(Color.TRANSPARENT)) {
+                return false;
+            }
+        }
+        return true;
 
     }
 
 
 }
+
